da8de907ac9dac4a0daa204bdea2d89e1ef12a8d
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-06-05 18:54:09 +0800

    recoded added json

--- app.js
+++ app.js
@@ -1,72 +1,110 @@
 // HEADER SLIDER
-const sliderContainer = document.querySelector('.header');
-let slides =  document.querySelectorAll('.slides');
-const navBtn = document.querySelectorAll('.btnNav');
-
+const sliderContainer = document.querySelector(".header");
+let slides = document.querySelectorAll(".slides");
+const navBtn = document.querySelectorAll(".btnNav");
+let showcase = document.querySelector('.showcase');
 console.log(slides);
 
-slides.forEach((item,index)=>{
+slides.forEach((item, index) => {
     console.log(item);
     item.style.left = `${index * 100}%`;
 });
 
-navBtn.forEach((btn,index)=>{
-    btn.addEventListener('click',e=>{
+navBtn.forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
         console.log(e.target);
-        e.target.style.backgroundColor = 'red';
-        theSlide =document.querySelector(`.slide${index+1}`);
-        theSlide.style.transform = `translate(-${index* 100}%)`
+        e.target.style.backgroundColor = "red";
+        theSlide = document.querySelector(`.slide${index + 1}`);
+        theSlide.style.transform = `translate(-${index * 100}%)`;
 
-        if (getComputedStyle(theSlide).left > 0){
+        if (getComputedStyle(theSlide).left > 0) {
             theSlide.style.left = 0;
         }
     });
 });
 
 // THEME
-const cardTile = document.querySelectorAll('.card');
-const cardText = document.querySelectorAll('.card__text');
-const themeBtn = document.querySelector('.themeBtn');
-const themeBtnSwitch = document.querySelector('.themeBtn__switch');
+const themeBtn = document.querySelector(".themeBtn");
+const themeBtnSwitch = document.querySelector(".themeBtn__switch");
+const filterInput = document.querySelector(".search__filter");
 
+themeBtn.addEventListener("click", () => {
+    const cardText = document.querySelectorAll(".card__text");
 
-themeBtn.addEventListener('click',()=>{
-    themeBtn.classList.toggle('lightMode');
-    themeBtnSwitch.classList.toggle('moveSwitch');
-    cardText.forEach(current=>{
-        current.classList.toggle('lightTheme');
+    themeBtn.classList.toggle("lightMode");
+    themeBtnSwitch.classList.toggle("moveSwitch");
+    cardText.forEach((current) => {
+        current.classList.toggle("lightTheme");
     });
-    document.querySelector('body').classList.toggle('lightTheme');
+    document.querySelector("body").classList.toggle("lightTheme");
 });
 
 // SEARCH
 
-let filterInput = document.querySelector('.search__filter');
 
-filterInput.addEventListener('change',()=>{
-    console.log(filterInput.value);
-    cardTile.forEach(card=>{
-        if (filterInput.value === 'all'){
-            card.style.display = 'block';
-        }
-        else if (card.dataset.level === filterInput.value){
-            card.style.display = 'block';
-        }
-        else{
-            card.style.display = 'none';
+
+async function searchCard() {
+    const data = await getData();
+    const cardTile = data.data;
+    let searchInput = document.querySelector(".search__input").value;
+
+    showcase.innerHTML = '';
+    cardTile.forEach((currentCard) => {
+        if (currentCard.title.toLowerCase().includes(searchInput.toLowerCase())) {
+            renderCard(currentCard);
         }
     });
-});
-document.querySelector('.search__input').addEventListener('keyup',searchCard);
+}
 
-function searchCard(){
-    let searchInput = document.querySelector('.search__input').value;
-    cardTile.forEach((currentCard,index)=>{
-        if(currentCard.dataset.name.includes(searchInput)){
-            currentCard.style.display = 'block';
-        }
-        else{
-            currentCard.style.display = 'none';
+async function getData() {
+    const data = await fetch(`./data.json`);
+    const result = await data.json();
+    return result;
+}
+
+async function renderAllCards() {
+    const data = await getData();
+    const challenges = data.data;
+    console.log(challenges);
+    challenges.forEach(data =>{
+        renderCard(data);
+    });
+}
+
+async function renderCard(challenge){
+    const markup = `
+    <div class="card" data-level='${challenge.level}' data-name='${challenge.title}'>
+        <div class="card__cover">
+            <a target='_blank' href="${challenge.demo_link}"><img src="${challenge.img}" alt="" class="card__img"></a>
+        </div>
+        <div class="card__text">
+            <h2 class="card__title">${challenge.title}</h2>
+            <p class="card__level">${challenge.level}</p>
+            <div class="card__links">
+                <a target='_blank' href="${challenge.demo_link}" class="card__link"><i class="fas fa-globe"></i></a>
+                <a target='_blank' href="${challenge.repo_link}" class="card__link"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+    </div>
+    `;
+
+    showcase.insertAdjacentHTML("beforeend", markup);
+
+}
+
+async function filterCards(){
+    const data = await getData();
+    const cards = data.data;
+    showcase.innerHTML = '';
+    cards.forEach(card =>{
+        if(card.level === filterInput.value){
+            renderCard(card);
+        }else if (filterInput.value === 'all'){
+            renderAllCards();
         }
     });
-};
+}
+
+window.addEventListener("DOMContentLoaded", renderAllCards);
+document.querySelector(".search__input").addEventListener("keyup", searchCard);
+filterInput.addEventListener("change", filterCards);
